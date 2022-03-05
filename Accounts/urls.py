f8c4869c10425eacdfdfed6a8158e79d7a7a02c5
UWIZEYE Jean de DIeu <jeandedieuuwizeye6@gmail.com>
2022-03-05 09:53:38 +0200

    finish authentication with django

---
+++ Accounts/urls.py
@@ -0,0 +1,15 @@
+
+from django.urls import include, path
+
+from . import views
+
+from rest_framework_simplejwt import views as jwt_views
+
+urlpatterns = [
+    
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('', views.UserListView.as_view()),
+]
+
+    
