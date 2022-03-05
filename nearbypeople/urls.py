648b9e0cef1548a3b8952f609be0fe59929ab828
UWIZEYE Jean de DIeu <jeandedieuuwizeye6@gmail.com>
2022-03-05 11:18:04 +0200

    fix urls

--- nearbypeople/urls.py
+++ nearbypeople/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('users/', include('Accounts.urls')),
+    path('message', include('messaging.urls')),
     path('rest-auth/', include('rest_auth.urls')),
 ]
